d6c80dd83e7e021953ce3e8dca83b373e79daf09
B20DCCN728 <vietjohnny810@gmail.com>
2024-01-06 10:03:49 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- App.vue -->
+
+<template>
+  <div id="app">
+    <h1>PMC Wagner Group</h1>
+		<div class="editor-wrapper"> 
+        <ContentEditor/>
+        </div> 
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ContentEditor from './components/editors/ContentEditor.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue';
+import App from './App.vue';
+// import CKEditor from '@ckeditor/ckeditor5-vue';
+const app = createApp(App);
+
+app.mount('#app');
